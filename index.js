8a112eafaa624f1c345a41a0caa132c79e3f0036
AdaN6 <a_disney_da@hotmail.com>
2022-11-17 14:56:54 +0100

    added redirect and making it dynamic

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 import express from "express";
 const app = express();
-const port = process.env.PPRT || 3000;
+const port = process.env.PORT || 3000;
 
 app
 .route('/')
-.get("/", (req, res) => {
-  res.send("This is a get request");
-});
-post("/", (req, res) => {
-  res.send("Hello this is a post request!");
-});
-put("/", (req, res) => {
-  res.send("Hello this is a put request");
-});
-delete("/", (req, res) => {
-  res.send("Hello this is a delete request");
-});
-listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+.get((req, res) => {
+  res.send("This is a get request")
+})
+.post((req, res) => {
+  res.send("Hello this is a post request!")
+})
+.put((req, res) => {
+  res.send("Hello this is a put request")
+})
+.delete((req, res) => {
+  res.send("Hello this is a delete request")
+})
+
+// redirect example
+
+app.get('/getexample', (req,res) => {res.redirect("/")})
+
+//making it dynamic
+
+app.get('/users/:id', (req, res) => {
+    console.log(req.params.id);
+    console.log(req.query.sort);
+    res.send(
+     `req.params is id and the value is ${req.params.id}\n req.query is ${req.query.sort}`);
+    })
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
 });
